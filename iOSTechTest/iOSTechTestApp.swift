7b06387d5821a796ba14275838051c38f52e5a33
Brandon Aubrey <baubrey91@gmail.com>
2025-01-28 17:33:39 -0800

    Update to workspace

--- iOSTechTest/iOSTechTestApp.swift
+++ iOSTechTest/iOSTechTestApp.swift
@@ -9,9 +9,27 @@ import SwiftUI
 
 @main
 struct iOSTechTestApp: App {
+    
     var body: some Scene {
         WindowGroup {
             BookListView()
+                .onAppear {
+                    configureURLlCache()
+                }
         }
     }
+    
+    // This set up should be in a scene delegate or app delegate
+    private func configureURLlCache() {
+        // Might be excessive but extra memory for image caching
+        let memoryCapacity = 50_000_000 // ~10 MB memory space
+        let diskCapacity = 1_000_000_000 // ~1GB disk cache space
+        let cache = URLCache(
+            memoryCapacity: memoryCapacity,
+            diskCapacity: diskCapacity,
+            diskPath: "techTestCache"
+        )
+        URLCache.shared = cache
+    }
 }
+
